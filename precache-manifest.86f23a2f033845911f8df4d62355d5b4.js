dd31e101121aa5f0d13b901cd1a18e0ab76c86f8
artem <platonovartem2005@outlook.com>
2021-02-22 19:19:38 +0300

    intitial

---
+++ precache-manifest.86f23a2f033845911f8df4d62355d5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4404db11fa1df8e8d046630112b43c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b8a87e9e7afc0326560",
+    "url": "/static/css/main.ab7468aa.chunk.css"
+  },
+  {
+    "revision": "795e9257e7059c2659d6",
+    "url": "/static/js/2.589a7f25.chunk.js"
+  },
+  {
+    "revision": "a3d04cefa1287aa2ab6d2aaae2a3b99f",
+    "url": "/static/js/2.589a7f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8a87e9e7afc0326560",
+    "url": "/static/js/main.f2532a1d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
